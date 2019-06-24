aab4e581ca9f499faec0413e8e970ed782960b24
realforceman <realforceman@gamil.com>
2019-06-24 22:47:08 +0900

    test

--- src/main/java/com/example/demo/web/JournalController.java
+++ src/main/java/com/example/demo/web/JournalController.java
@@ -1,16 +1,25 @@
 package com.example.demo.web;
 
+import java.util.List;
+import com.example.demo.domain.Journal;
 import com.example.demo.repository.JournalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class JournalController {
 
     @Autowired
     JournalRepository repo;
+
+    @RequestMapping(value="/journal", produces={MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public @ResponseBody List<Journal> getJournal() {
+        return repo.findAll();
+    }
 
     @RequestMapping("/")
     public String index(Model model) {
